59d92cb7ba7179a6e72d363b93a62bd4fd08f250
Hiroki Yokoyama <javawriter.hy@gmail.com>
2021-01-04 15:00:48 +0900

    add camera image publisher

---
+++ nodes/camera_image_publisher.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+
+from sensor_msgs.msg import Image
+import cv2
+import cv_bridge
+
+class CameraImagePublisher(Node):
+    def __init__(self):
+        super().__init__('camera_image_publisher')
+        
+        # Start publisher
+        self.pub = self.create_publisher(Image, '/image', 1)
+        self.cv_bridge = cv_bridge.CvBridge()
+
+    def spin(self):
+        cap = cv2.VideoCapture(1)
+        
+        while rclpy.ok():
+            ret, img = cap.read()
+            if no ret:
+                self.get_logger().fatal('Cannot capture camera image.')
+                break
+            msg = cv2_to_imgmsg(img)
+            self.joint_states_pub.publish(msg)
+        
+def main(args=None):
+    rclpy.init(args=args)
+    try:
+        node = CameraImagePublisher()
+        node.spin()
+    finally:
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
